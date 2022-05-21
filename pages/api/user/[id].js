28c06e000c4348ebf13eea237b9f8b8f26519dce
Thang <haikhauak@gmail.com>
2022-05-21 23:22:57 +0700

    add CRUD api for user

---
+++ pages/api/user/[id].js
@@ -0,0 +1,94 @@
+import conn from '../../../lib/db'
+
+export default async function userHandler(req, res) {
+    const {
+      query: { id },
+      method,
+      body
+    } = req
+
+    const userData = {
+        userId: body.userId,
+        firstName: body.firstName,
+        lastName: body.lastName,
+        dob: body.dob,
+        gender: body.gender,
+        tel: body.tel,
+        email: body.email,
+        address: body.address,
+        imgUrl: body.imgUrl,
+    }
+  
+    switch (method) {
+      case 'GET':
+        try {
+            let query = 'SELECT * FROM users WHERE user_id = $1;'
+            let values = [id]
+            let result = await conn.query(query, values)
+            if (result.rows[0]) res.status(200).json(result.rows)
+            else res.status(400).json(`User with id ${id} cannot be found!`)
+        } catch (error) {
+            console.log(error)
+            res.status(500).json("Something went wrong, try again!")
+        }
+        break
+      case 'PATCH':
+        try {
+            let query = 'SELECT * FROM users WHERE user_id = $1'
+            let values = [id]
+            let isExisted = await conn.query(query, values)
+            if (isExisted.rows[0]) {
+                try {
+                    let query = 'UPDATE users SET first_name = $1, last_name = $2, dob = $3, gender = $4, tel = $5, email = $6, address = $7, img_url = $8 WHERE user_id = $9'
+                    let values = [
+                        userData.firstName,
+                        userData.lastName,
+                        userData.dob,
+                        userData.gender,
+                        userData.tel,
+                        userData.email,
+                        userData.address,
+                        userData.imgUrl,
+                        id
+                    ]
+                    let result = await conn.query(query, values)
+                    res.status(200).json(`User with id ${id} has been updated!`)
+                } catch (error) {
+                    console.log(error)
+                    res.status(500).json("Something went wrong, try again! 1")
+                }
+            } else {
+                res.status(400).json(`User with id ${id} cannot be found!`)
+            }
+        } catch (error) {
+            console.log(error)
+            res.status(500).json("Something went wrong, try again! 2")
+        }
+        break
+        case 'DELETE':
+            try {
+                let query = 'SELECT * FROM users WHERE user_id = $1'
+                let values = [id]
+                let isExisted = await conn.query(query, values)
+                if (isExisted.rows[0]) {
+                    try {
+                        let query = 'DELETE FROM users WHERE user_id = $1'
+                        let values = [id]
+                        let result = await conn.query(query, values)
+                        res.status(200).json(`User with id ${id} has been deleted!`)
+                    } catch (error) {
+                        res.status(500).json("Something went wrong, try again!")
+                    }
+                } else {
+                    res.status(400).json(`User with id ${id} cannot be found!`)
+                }
+            } catch (error) {
+                    console.log(error)
+                    res.status(500).json("Something went wrong, try again!")
+            }
+            break
+      default:
+        res.setHeader('Allow', ['GET', 'PATCH', 'DELETE'])
+        res.status(405).end(`Method ${method} Not Allowed`)
+    }
+  }
---
+++ pages/api/user/index.js
@@ -0,0 +1,60 @@
+import conn from '../../../lib/db'
+
+// create new user
+export default async function userHandler(req, res) {
+    const { method, body } = req
+
+    const userData = {
+        userId: body.userId,
+        firstName: body.firstName,
+        lastName: body.lastName,
+        dob: body.dob,
+        gender: body.gender,
+        tel: body.tel,
+        email: body.email,
+        address: body.address,
+        imgUrl: body.imgUrl,
+    }
+
+    switch (method) {
+        case 'GET':
+            try {
+                // const query = 'SELECT user_id, first_name, last_name, dob, gender, email, address, borrow_times, img_url, tel FROM users;' duh...
+                let query = 'SELECT * FROM users;'
+                let result = await conn.query(query)
+                res.status(200).json(result.rows)
+            } catch (error) {
+                console.log(error)
+                res.status(500).res.status(500).json("Something went wrong, try again!")
+            }
+            break
+        case 'POST':
+            try {
+                let query = 'INSERT INTO users(first_name, last_name, dob, gender, tel, email, address, img_url) VALUES($1, $2, $3, $4, $5, $6, $7, $8)'
+                let values = [
+                    userData.firstName,
+                    userData.lastName,
+                    userData.dob,
+                    userData.gender,
+                    userData.tel,
+                    userData.email,
+                    userData.address,
+                    userData.imgUrl
+                ]
+                let result = await conn.query(
+                    query,
+                    values
+                );
+                console.log("Result: ", result)
+                res.status(200).json(`User ${userData.firstName + ' ' + userData.lastName} has been created!`)
+            }
+            catch (error) {
+                console.log(error)
+                res.status(500).json("Something went wrong, try again!")
+            }
+            break
+        default:
+            res.setHeader('Allow', ['GET', 'POST'])
+            res.status(405).end(`Method ${method} Not Allowed`)
+    }
+};
